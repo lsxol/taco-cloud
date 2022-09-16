36d9f6e955bb885482730da55b81b136d1bd7d39
lsxol <d.kazmierczak96@gmail.com>
2022-09-16 12:41:48 +0200

    Taco, TacoOrder data validation

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,11 +3,13 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import tacos.domain.Ingredient;
 import tacos.domain.Taco;
 import tacos.domain.TacoOrder;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -56,8 +58,14 @@ public class DesignTacoController {
         return "design";
     }
 
-    @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+    @PostMapping                              
+    public String processTaco(@Valid Taco taco,
+                              Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder){
+
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}",taco);
 
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,12 +2,15 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.domain.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -19,8 +22,12 @@ public class OrderController {
     public String orderForm(){
         return "orderForm";
     }
+
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus){
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/home";
